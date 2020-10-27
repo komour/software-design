ee84e6d59f8b09de35b81d58625e76d9c52024b2
komour <komaroff404@gmail.com>
2020-10-27 09:22:57 +0300

    Hide the token KEKW

--- VKApiApp/src/main/kotlin/VkApi.kt
+++ VKApiApp/src/main/kotlin/VkApi.kt
@@ -1,7 +1,6 @@
 import java.io.File
 import java.net.URLEncoder
 
-private const val ACCESS_KEY = "2ba909f72ba909f72ba909f7a72bdd3cf722ba92ba909f774259b56ea457c2692f55a00"
 private const val API_VERSION = "5.124"
 private const val BASE_URL = "https://api.vk.com/method/newsfeed.search"
 
@@ -11,9 +10,33 @@ class VkApi {
         responseFile.writeText(readUrl(buildUrl(query, startTime)))
         return parse(readUrl(buildUrl(query, startTime)))
     }
-
     private fun buildUrl(query: String, startTime: Long): String {
         val q = URLEncoder.encode(query, "utf8")
         return "$BASE_URL?q=$q&start_time=$startTime&count=200&access_token=$ACCESS_KEY&v=$API_VERSION"
     }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+private const val ACCESS_KEY = "2ba909f72ba909f72ba909f7a72bdd3cf722ba92ba909f774259b56ea457c2692f55a00"
